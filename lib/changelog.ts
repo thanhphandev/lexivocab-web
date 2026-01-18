ee3372234ef85b64aa8e04d0daafc78cdfba220f
Phan Văn Thành <thanhphanvan1610@gmail.com>
2026-01-18 16:10:41 +0700

    docs: update changelog for release v1.1.0

--- lib/changelog.ts
+++ lib/changelog.ts
@@ -1,5 +1,14 @@
 export const versions = [
     {
+        version: "1.1.0",
+        date: "2026-01-18",
+        changes: [
+            "mode_toggle_feature",
+            "realtime_input_translation",
+            "highlight_performance_optimization"
+        ]
+    },
+    {
         version: "1.0.0",
         date: "2026-01-08",
         changes: ["initial_release"]
